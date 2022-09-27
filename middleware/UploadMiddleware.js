76da0371a704015672f49b08a104576168f6170e
xuannhutzz <xuannhutzz@gmail.com>
2022-09-27 23:53:37 +0700

    update@introduce: add file readme introduce BE

--- middleware/UploadMiddleware.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-const upload = multer({
-    limits: {
-        fileSize: 4*1024*1024 //bytes->4MB
-    }
-})
-
-export { upload };
--- utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (id, secret, expiresIn) => {
-  return jwt.sign({ id }, secret, {
-    expiresIn,
-  });
-};
-
-export default generateToken;
